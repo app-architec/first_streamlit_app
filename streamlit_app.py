f67fd8bdca915eb341827fc0fa121e0a8637b2cf
app-architec <142472753+app-architec@users.noreply.github.com>
2023-08-18 13:18:20 -0500

    Update streamlit_app.py
    Add my fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,6 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-
+# Allow the end user to add fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
