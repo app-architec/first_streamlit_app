39663f209c65269ca82e801b1856aded7d74cc59
app-architec <142472753+app-architec@users.noreply.github.com>
2023-08-17 21:59:56 -0500

    Update streamlit_app.py
    JSON in table format with pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#REST API
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalize the JSON response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write you JSON as table
+streamlit.dataframe(fruityvice_normalized)
